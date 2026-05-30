6c2c3db5dfd8bd3a49ccec7ee7fe51f174fcd003
Levlvovich1 <levlogvinov@gmail.com>
2026-05-30 23:06:34 +0300

    os - windows; added links

--- setup.py
+++ setup.py
@@ -8,21 +8,21 @@ def readme():
 
 setup(
     name="ivaoauroraconnect",
-    version="0.5",
+    version="1.0",
     author="LevLvovich1",
     author_email="levlogvinov@gmail.com",
     description="This is library for work with IVAO Aurora",
     long_description=readme(),
     long_description_content_type="text/markdown",
-    url="",
+    url="http://team-majula-projects.ru:1788/",
     packages=find_packages(),
     install_requires=[],
     classifiers=[
         "Programming Language :: Python :: 3.13",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Operating System :: Microsoft :: Windows",
     ],
     keywords="ivao",
-    project_urls={"GitHub": ""},
+    project_urls={"GitHub": "https://github.com/LevLvovich1/ivaoauroraconnect"},
     python_requires=">=3.13",
 )
